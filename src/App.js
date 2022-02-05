f91117dfbb0abbf1149b818839fb5a5568c08567
Mher <mher2202@gmail.com>
2022-02-05 17:00:08 +0400

    Played a bit with states

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { APP_DATA } from "./constants";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
-function App() {
+const App = () => {
+  console.log("App");
+  const [pageData, setPageData] = useState(APP_DATA);
+  const [loading, setLoading] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          width="150px"
-          height="150px"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactasdasdasdasd
-        </a>
-      </header>
+    <div style={{ width: "50vw", margin: "auto" }}>
+      <h1>{pageData.title}</h1>
+      {loading && <h1>loading</h1>}
+      {pageData.isLogedIn ? (
+        <h2>hello {pageData.userName}</h2>
+      ) : (
+        <>
+          <p id="my_p">123</p>
+          <h2>Please Sign in</h2>
+        </>
+      )}
+
+      <pre>{JSON.stringify(pageData, true, 4)}</pre>
+      <TextField
+        id="outlined-basic"
+        onChange={(e) => setPageData({ ...pageData, title: e.target.value })}
+        label="Outlined"
+        variant="outlined"
+        value={pageData.title}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        size="medium"
+        onClick={() => {
+          document.getElementById("my_p").innerHTML = "Hacked";
+        }}
+      >
+        Click messs
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        size="medium"
+        onClick={() => {
+          setPageData({ ...pageData, isLogedIn: !pageData.isLogedIn });
+          setLoading(true);
+          setTimeout(() => setLoading(false), 2000);
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const APP_DATA = {
+  title: "someTitle",
+  isLogedIn: false,
+  userName: "John",
+};
