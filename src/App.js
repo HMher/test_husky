266d25316ec369df7b85f0a7f20dd5aec1ac4abb
Mher <mher2202@gmail.com>
2022-02-05 13:59:35 +0400

    asdasdas

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactasd asdasdasdasd
+          Learn Reactasd
         </a>
       </header>
     </div>
